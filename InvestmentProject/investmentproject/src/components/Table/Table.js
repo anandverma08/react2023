ccebf74bab99cb115d794ea0d11fcee821759d69
Anand,Vermam <anandverma552@outlook.comm>
2023-07-07 21:03:02 -0400

    Currency symbol added

--- InvestmentProject/investmentproject/src/components/Table/Table.js
+++ InvestmentProject/investmentproject/src/components/Table/Table.js
@@ -19,10 +19,10 @@ export default function Table(props) {
             return (
                 <tr>
               <td>{yearData.year}</td>
-              <td>${yearData.savingsEndOfYear.toFixed(2)}</td>
-              <td>{yearData.yearlyInterest.toFixed(2)}</td>
-              <td>{yearData.savingsEndOfYear.toFixed(2) - props.initialSaving.toFixed(2) - yearData.yearlyContribution.toFixed(2)*yearData.year.toFixed(2)}</td>
-              <td>{props.initialSaving + yearData.yearlyContribution*yearData.year}</td>
+              <td>$ {yearData.savingsEndOfYear.toFixed(2)}</td>
+              <td>$ {yearData.yearlyInterest.toFixed(2)}</td>
+              <td>$ {(yearData.savingsEndOfYear - props.initialSaving - yearData.yearlyContribution*yearData.year).toFixed(2)}</td>
+              <td>$ {props.initialSaving + yearData.yearlyContribution*yearData.year}</td>
               </tr>
             )
           })}
